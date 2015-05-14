1bf47b9b4cb5b9edcdd112077fb8729f1c7c78a6
safari <sshsfl@yeah.net>
2015-05-14 17:54:54 +0800

    add comment method

--- app/Http/Controllers/OpInfoController.php
+++ app/Http/Controllers/OpInfoController.php
@@ -9,12 +9,28 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class OpInfoController extends BaseController
 {
-    // comment
-    // todo
+    /**
+     *
+     * @param  int $id
+     * @return class OpInfoComment
+     */
     public function comment($id)
     {
     	$content = Request::input('content');
-    	return OpInfoComment::find(3);
-    	// todo
+
+		DB::beginTransaction();
+		// insert into dbt
+		$opInfoComment             = new OpInfoComment();
+		$opInfoComment->uid        = 21;
+		$opInfoComment->op_info_id = $id;
+		$opInfoComment->content    = $content;
+		$opInfoComment->save();
+		// update dbt
+		$opInfo = OpInfo::find($id);
+		$opInfo->comment += 1;
+		$opInfo->save();
+
+		DB::commit();
+		return $opInfoComment;
     }
 }

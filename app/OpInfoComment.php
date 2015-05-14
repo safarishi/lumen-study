ad5a5ee04a74430b5e029165580a4d541e37f48a
safari <sshsfl@yeah.net>
2015-05-14 17:13:31 +0800

    orm model

---
+++ app/OpInfoComment.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Database\Eloquent\Model;
+
+class OpInfoComment extends Model
+{
+	/**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'op_info_comment';
+}

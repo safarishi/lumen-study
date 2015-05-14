c9c53912888d4ef2c9bc95fe7eda43a8ac5cf8c6
safari <sshsfl@yeah.net>
2015-05-14 17:53:45 +0800

    op_info model add

---
+++ app/OpInfo.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Database\Eloquent\Model;
+
+class OpInfo extends Model
+{
+	/**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'op_info';
+}

077d4e09c7e84424c04b1c58aacf730df956178a
safari <sshsfl@yeah.net>
2015-05-14 17:54:15 +0800

    modify code style

--- app/OpInfoComment.php
+++ app/OpInfoComment.php
@@ -5,9 +5,9 @@ use Illuminate\Database\Eloquent\Model;
 class OpInfoComment extends Model
 {
 	/**
-     * The database table used by the model.
-     *
-     * @var string
-     */
-    protected $table = 'op_info_comment';
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'op_info_comment';
 }

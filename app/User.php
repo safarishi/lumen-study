a8f128ca82d50ae116b719268766df02ca73ae8a
safari <sshsfl@yeah.net>
2015-05-15 13:48:22 +0800

    commit error

---
+++ app/User.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+	/**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
+}
